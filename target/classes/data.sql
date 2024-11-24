4561709a1e4cbee2ff8f42d7a8a8da235197d371
SayatErtufenk <sayat.ert@gmail.com>
2024-11-25 02:18:45 +0300

    First Commit

--- target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO customer (name, surname, username, credit_limit, used_credit_limit)
-VALUES ('Sayat', 'Ertüfenk', 'sayti', 50000.00, 0.00);
-
-INSERT INTO customer (name, surname, username, credit_limit, used_credit_limit)
-VALUES ('Adife', 'Ertüfenk', 'adis', 30000.00, 0.00);
-
-INSERT INTO customer (name, surname, username, credit_limit, used_credit_limit)
-VALUES ('test', 'customer', 'customer', 50000.00, 0.00);
